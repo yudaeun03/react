7921497184ea9fae64dbe187903c8431586c207e
yudaeun03 <appae6772@naver.com>
2023-10-30 21:04:20 +0900

    exam04 commit

--- demo03/src/components/Exam03.js
+++ demo03/src/components/Exam03.js
@@ -2,22 +2,12 @@ import { useState } from 'react';
 
 
 
-function Exam03(){
-    const[number, setNumber] = useState(0);
+// function Exam03(){
 
-    function one() {
-        setNumber(number + 10000);
-    }
-    function five() {
-        setNumber(number + 50000);
-    }
-    function ten() {
-        setNumber(number + 100000);
-    }
-    function reset() {
-        setNumber(0);
-    }
-    
+    const Exam03 = ()=> {
+    const [money, setMoney] = useState(0);
+
+
 
 
 return (
@@ -25,13 +15,19 @@ return (
     <h1>세 번째 예제</h1>
 
     <h3> 출금금액 :  </h3>
-    <h4> {number}원</h4>
+    <h4> {money}원</h4>
     <br/>
-    <button onClick={one}> 1만원 </button>
-    <button onClick={five}> 5만원 </button>
-    <button onClick={ten}> 10만원 </button>
-    <button onClick={reset}> 초기화</button>
-    <br/><br/>
+    <button className ="btn btn-primary me-1" onClick={()=>setMoney(money + 100000)}> 10만원 </button>
+    <button className ="btn btn-primary me-1" onClick={()=>setMoney(money + 50000)}> 5만원 </button>
+    <button className ="btn btn-primary me-1" onClick={()=>setMoney(money + 10000)}> 1만원 </button>
+    <button className ="btn btn-primary me-1" onClick={()=>setMoney(money + 0)}> 초기화</button>
+    <br/>
+    <input 
+    type="range" 
+    min="0" 
+    max="10000000" 
+    step="10000" value={money}
+    onChange={e=>setMoney(parseInt(e.target.value))}/>
     </>
 
     );
