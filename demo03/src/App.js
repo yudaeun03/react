014ba6bac4e891dc32629e5869acd48f08716da1
yudaeun03 <appae6772@naver.com>
2023-10-30 16:22:33 +0900

    Exam파일 업로드

---
+++ demo03/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Exam01 from './components/Exam01';
+
+function App() {
+  return (
+    <>
+   <h1> 각종 리액트 예제 </h1>
+
+   <Exam01/>
+   </>
+  );
+}
+
+export default App;
