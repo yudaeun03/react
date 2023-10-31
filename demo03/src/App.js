6e629ae78fa5470dfe40cedc416737bf8cbf4c16
yudaeun03 <appae6772@naver.com>
2023-10-31 14:15:28 +0900

    Exam05. 06. 07 commit

--- demo03/src/App.js
+++ demo03/src/App.js
@@ -4,10 +4,13 @@ import Exam01 from './components/Exam01';
 import Exam02 from './components/Exam02';
 import Exam03 from './components/Exam03';
 import Exam04 from './components/Exam04';
+import Exam05 from './components/Exam05';
+import Exam06 from './components/Exam06';
+import Exam07 from './components/Exam07';
 
 function App() {
   return (
-    <>
+    <>s
    <h1> 각종 리액트 예제 </h1>
 
    <Exam01/>
@@ -17,6 +20,12 @@ function App() {
    <Exam03/>
    <hr/>
    <Exam04/>
+   <hr/>
+   <Exam05/>
+   <hr/>
+   <Exam06/>
+   <hr/>
+   <Exam07/>
    <hr/>
    </>
   );
